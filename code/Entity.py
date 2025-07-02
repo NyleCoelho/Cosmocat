c8a37a1b91f3aa6e4e9b03d41fe44d8de9202fe7
nylesuik <biancacoelhoo05@gmail.com>
2025-07-02 13:07:47 -0400

    Add alpha channel support for background images in Entity class

--- code/Entity.py
+++ code/Entity.py
@@ -6,7 +6,7 @@ class Entity(ABC):
 
     def __init__(self, name: str, position: tuple):
         self.name = name
-        self.surf = pygame.image.load('./assets/Backgrounds/level1/' + name + '.png')
+        self.surf = pygame.image.load('./assets/Backgrounds/level1/' + name + '.png').convert_alpha()
         self.surf = pygame.transform.scale(self.surf, (WIN_WIDTH, WIN_HEIGHT))
         self.rect = self.surf.get_rect(left=position[0], top=position[1])
         self.speed = 0
