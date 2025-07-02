10c9c9bc505cdb7f483a249d21df56488a39ba9f
nylesuik <biancacoelhoo05@gmail.com>
2025-07-02 17:32:02 -0400

    Refactor Entity and Player classes to include scale_to_screen parameter for flexible background scaling

--- code/Entity.py
+++ code/Entity.py
@@ -3,12 +3,16 @@ import pygame.image
 from code.Const import WIN_WIDTH, WIN_HEIGHT
 
 class Entity(ABC): 
-
-    def __init__(self, name: str, position: tuple):
+    def __init__(self, name: str, position: tuple, scale_to_screen=True):
         self.name = name
-        self.surf = pygame.image.load('./assets/Backgrounds/level1/' + name + '.png').convert_alpha()
-        self.surf = pygame.transform.scale(self.surf, (WIN_WIDTH, WIN_HEIGHT))
-        self.rect = self.surf.get_rect(left=position[0], top=position[1])
+        self.surf = pygame.image.load(f'./assets/Backgrounds/level1/{name}.png').convert_alpha()
+        
+        if scale_to_screen:
+            self.surf = pygame.transform.scale(self.surf, (WIN_WIDTH, WIN_HEIGHT))
+        else:
+            self.surf = pygame.transform.scale(self.surf, (95, 95))  
+
+        self.rect = self.surf.get_rect(topleft=position)
         self.speed = 0
 
     @abstractmethod
--- code/Player.py
+++ code/Player.py
@@ -1,13 +1,11 @@
 from code.Entity import Entity
 
 class Player(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position, scale_to_screen=False)
 
-    def __init__ (self, name: str, position: tuple):
-        super().__init__(name, position)
-        
-
-    def update(self, ):
+    def update(self):
         pass
 
-    def move (self, ):
+    def move(self):
         pass
