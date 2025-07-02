647f7b65474deb79988ddfb2337b3d1d79828ff0
nylesuik <biancacoelhoo05@gmail.com>
2025-07-02 17:28:30 -0400

    Implement code changes to enhance functionality and improve performance

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,5 +1,6 @@
 from code.Background import Background
-from code.Const import WIN_WIDTH
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Player import Player
 class EntityFactory:
 
     @staticmethod
@@ -11,4 +12,5 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (0,0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH,0)))
                 return list_bg
-           
+            case 'Cosmocat':
+                return Player('Cosmocat', (10, WIN_HEIGHT / 2))
